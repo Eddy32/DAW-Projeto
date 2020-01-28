77ca660be15633b10831e4772133b39e08d14c91
ineesalvees <inesamorimalves@gmail.com>
2020-01-28 21:31:24 +0000

    frontend

--- FrontEnd/public/assets/js/argon.js
+++ FrontEnd/public/assets/js/argon.js
@@ -1061,3 +1061,36 @@ var SalesChart = (function() {
 	}
 
 })();
+
+;(function($){
+  
+    /**
+     * Store scroll position for and set it after reload
+     *
+     * @return {boolean} [loacalStorage is available]
+     */
+    $.fn.scrollPosReaload = function(){
+        if (localStorage) {
+            var posReader = localStorage["posStorage"];
+            if (posReader) {
+                $(window).scrollTop(posReader);
+                localStorage.removeItem("posStorage");
+            }
+            $(this).click(function(e) {
+                localStorage["posStorage"] = $(window).scrollTop();
+            });
+
+            return true;
+        }
+
+        return false;
+    }
+    
+    /* ================================================== */
+
+    $(document).ready(function() {
+        // Feel free to set it for any element who trigger the reload
+        $('select').scrollPosReaload();
+    });
+  
+}(jQuery));  
